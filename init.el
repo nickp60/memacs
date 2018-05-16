3acfc0e744966dd7e47619035e0cd49a5cabf532
Nicholas Waters <nickp60@gmail.com>
2018-05-16 15:25:10 +0100

    re-init commit after torching the old repo

---
+++ init.el
@@ -0,0 +1,30 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-engine (quote xetex))
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(ess-swv-processor (quote knitr))
+ '(inhibit-startup-buffer-menu t)
+ '(inhibit-startup-screen t)
+ '(org-confirm-babel-evaluate nil)
+ '(package-selected-packages
+   (quote
+    (highlight-chars multiple-cursors ox-reveal exec-path-from-shell pycoverage ox-gfm org markdown-mode jade-mode auto-complete-auctex auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 110 :family "inconsolata")))))
