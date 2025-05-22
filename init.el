4fe43ca139456a2c5b7b1e25b89efadf005a3102
Nick Waters <nickp60@users.noreply.github.com>
2025-05-22 10:21:00 -0400

    more updates from local

--- init.el
+++ init.el
@@ -6,23 +6,26 @@
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(org-babel-load-file "~/.emacs.d/local.org")
+;;(org-babel-load-file "~/.emacs.d/local.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-engine (quote xetex))
+ '(TeX-engine 'xetex)
  '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (manoj-dark)))
- '(ess-swv-processor (quote knitr))
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9"
+    "white"])
+ '(custom-enabled-themes '(manoj-dark))
+ '(ess-swv-processor 'knitr)
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-screen t)
  '(org-confirm-babel-evaluate nil)
  '(package-selected-packages
-   (quote
-    (highlight-chars multiple-cursors ox-reveal exec-path-from-shell pycoverage ox-gfm org markdown-mode jade-mode auto-complete-auctex auctex))))
+   '(ac-ispell auctex auto-complete-auctex ess exec-path-from-shell
+	       highlight-chars jade-mode marginalia markdown-mode
+	       multiple-cursors org ox-gfm ox-reveal pycoverage
+	       snakemake-mode vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
